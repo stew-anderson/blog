6aa0b1f4f53a5bea70a73201d73b362a0eb3e5b0
stew-anderson <stewtech@stewart-anderson.co.uk>
2025-09-25 15:35:53 +0100

    Adding template to style results

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,42 @@
+let blogsearch = blogsearch || {};
+
+/**
+ * findPosts
+ * 
+ * @param {*} query 
+ * @param {*} posts 
+ */
+blogsearch.findPosts = function (query, posts) {
+
+    posts.filter(post =>
+      post.title.toLowerCase().includes(query) ||
+      post.description.toLowerCase().includes(query) || 
+      post.tags.toLowerCase().includes(query)
+    );
+}
+
+/**
+ * render
+ * 
+ * @returns string
+ */
+blogsearch.render = function () {
+
+  return `
+    <article class="post">
+      <a class="post-thumbnail" style="background-image: url(${post.img})" href="${post.url}"></a>
+      <div class="post-content">
+        <h2 class="post-title"><a href="${post.url}">${post.title}</a></h2>
+        <p>${post.excerpt || ''} <a href="${post.url}">Read more</a></p>
+        <span class="post-date">${post.date}&nbsp;&nbsp;&nbsp;—&nbsp;</span>
+      </div>
+    </article>
+    `;
+}
+
+/**
+ * 
+ */
 document.addEventListener('DOMContentLoaded', function () {
   const searchInput = document.getElementById('search-input');
   const resultsContainer = document.getElementById('search-results');
@@ -14,16 +53,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (query.length < 2) return;
 
-    const filtered = posts.filter(post =>
-      post.title.toLowerCase().includes(query) ||
-      post.description.toLowerCase().includes(query) || 
-      post.tags.toLowerCase().includes(query)
-    );
+    const filtered = blogsearch.findPosts(query, posts);
 
     filtered.forEach(post => {
-      const item = document.createElement('div');
+      const item = document.createElement('article');
 
-      item.innerHTML = '<a href='+post.url+'">'+post.title+' | '+post.date+' | '+post.description+' | '+post.tags+'</a>';
+      item.addClass('post');
+      item.innerHTML = blogsearch.render(post);
       resultsContainer.appendChild(item);
     });
   });
